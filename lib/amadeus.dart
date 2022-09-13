845e43994e38744df1be2faefa49f0a73034b135
Ian Koerich Maciel <ianmaciel@gmail.com>
2022-09-13 18:59:20 -0300

    Initial commit

---
+++ lib/amadeus.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library amadeus;
+
+export 'src/amadeus_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
